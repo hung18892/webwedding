fb1e8c03cfc994f17d986343227a5c69842d593a
hung18892 <tranquochung.18892@gmail.com>
2025-07-17 16:47:04 +0700

    Update Gifts.jsx

--- src/pages/Gifts.jsx
+++ src/pages/Gifts.jsx
@@ -28,7 +28,7 @@ export default function Gifts() {
   const handleQrClick = (bankIndex, e) => {
     e.stopPropagation();
     // Hiển thị ảnh tương ứng với ngân hàng
-    setCurrentImage(bankIndex === 0 ? '/images/image5.jpg' : '/images/image6.jpg');
+    setCurrentImage(bankIndex === 0 ? '/images/image6.jpg' : '/images/image7.jpg');
   };
 
   const closeImageModal = () => {
