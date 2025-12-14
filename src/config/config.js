b4ce2e7cee89f11b276237e389b0db1ef916e475
hung18892 <tranquochung.18892@gmail.com>
2025-12-14 20:58:37 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,7 @@ const config = {
     maps_embed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.3101538280197!2d107.5921124!3d16.4680524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3141a13cb3f9bcfb%3A0x99aae25688f6df9c!2sTrung%20t%C3%A2m%20h%E1%BB%99i%20ngh%E1%BB%8B%20ti%E1%BB%87c%20c%C6%B0%E1%BB%9Bi%20Full%20House!5e0!3m2!1svi!2s!4v1721031000000!5m2!1svi!2s",
     time: "11:30 AM",
     location: "Nhà hàng tiệc cưới Fullhouse",
-    address: "45 Lê Lợi, Phú Hội, Huế, Thành phố Huế",
+    address: "45 Lê Lợi, phường Thuận Hóa, Thành phố Huế",
     ogImage: "/images/og-image.jpg",
     favicon: "/images/favicon.ico",
     agenda: [
@@ -23,7 +23,7 @@ const config = {
         startTime: "11:30",
         endTime: "14:30",
         location: "Nhà hàng tiệc cưới Fullhouse",
-        address: "45 Lê Lợi, Phú Hội, Huế, Thành phố Huế",
+        address: "45 Lê Lợi, phường Thuận Hóa, Thành phố Huế",
       },
       {
         title: "Lễ cưới",
